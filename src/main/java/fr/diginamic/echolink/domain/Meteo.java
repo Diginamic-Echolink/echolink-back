9d262ccae9eaffca151e2fa8d36efcec0a1950a4
Rohmane <R.S.T.E@hotmail.fr>
2026-06-01 16:38:09 +0200

    Chore: Classes and javadoc

--- src/main/java/fr/diginamic/echolink/domain/Meteo.java
+++ src/main/java/fr/diginamic/echolink/domain/Meteo.java
@@ -1,4 +1,124 @@
 package fr.diginamic.echolink.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Represents a weather record collected for a specific location.
+ * Stores meteorological data such as temperature, atmospheric pressure,
+ * humidity, wind information and rainfall measurements.
+ */
+@Entity
+@Getter
+@Setter
 public class Meteo {
+
+    /**
+     * Unique identifier of the weather record.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    /**
+     * Date and time when the weather data was recorded.
+     */
+    private LocalDateTime timestamp;
+
+    /**
+     * Measured temperature.
+     */
+    private int temp;
+
+    /**
+     * Measured atmospheric pressure.
+     */
+    private int atmPress;
+
+    /**
+     * Measured humidity level.
+     */
+    private int humidity;
+
+    /**
+     * Measured wind speed.
+     */
+    private int windSpeed;
+
+    /**
+     * Measured wind direction.
+     */
+    private int windDir;
+
+    /**
+     * Measured rainfall amount.
+     */
+    private int rain;
+
+    /**
+     * Location associated with this weather record.
+     */
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    /**
+     * Creates an empty weather record.
+     * Required by JPA.
+     */
+    public Meteo() {}
+
+    /**
+     * Creates a weather record.
+     *
+     * @param id unique identifier of the weather record
+     * @param timestamp date and time when the weather data was recorded
+     * @param temp measured temperature
+     * @param atmPress measured atmospheric pressure
+     * @param humidity measured humidity level
+     * @param windSpeed measured wind speed
+     * @param windDir measured wind direction
+     * @param rain measured rainfall amount
+     */
+    public Meteo(
+            UUID id,
+            LocalDateTime timestamp,
+            int temp,
+            int atmPress,
+            int humidity,
+            int windSpeed,
+            int windDir,
+            int rain) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.temp = temp;
+        this.atmPress = atmPress;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.windDir = windDir;
+        this.rain = rain;
+    }
+
+    /**
+     * Returns a string representation of the weather record.
+     *
+     * @return a string containing the weather data
+     */
+    @Override
+    public String toString() {
+        return "Meteo{" +
+                "id=" + id +
+                ", timestamp=" + timestamp +
+                ", temp=" + temp +
+                ", atmPress=" + atmPress +
+                ", humidity=" + humidity +
+                ", windSpeed=" + windSpeed +
+                ", windDir=" + windDir +
+                ", rain=" + rain +
+                '}';
+    }
 }
--- src/main/java/fr/diginamic/echolink/domain/Profile.java
+++ src/main/java/fr/diginamic/echolink/domain/Profile.java
@@ -1,44 +1,119 @@
 package fr.diginamic.echolink.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Represents a user profile within the EchoLink application.
+ * Contains personal information, contact details and user privileges.
+ */
 @Entity
 @Getter
 @Setter
 public class Profile {
 
+    /**
+     * Unique identifier of the profile.
+     */
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    /**
+     * User's first name.
+     */
     private String firstName;
 
+    /**
+     * User's last name.
+     */
     private String lastName;
 
+    /**
+     * User's public display name.
+     */
     private String pseudonym;
 
+    /**
+     * User's email address.
+     * Must be unique within the application.
+     */
+    @Column(nullable = false, unique = true)
     private String email;
 
+    /**
+     * User's city of residence.
+     */
     private String city;
 
-    private int postalCode;
+    /**
+     * User's postal code.
+     */
+    private String postalCode;
 
+    /**
+     * User's postal address.
+     */
     private String address;
 
+    /**
+     * User's phone number.
+     */
     private String phoneNumber;
 
+    /**
+     * URL of the user's profile picture.
+     */
     private String linkImgProfile;
 
-    private long meteoBookmarks;
-
+    /**
+     * Indicates whether the user has administrator privileges.
+     */
     private boolean admin;
 
+    /**
+     * Threads created by this user.
+     */
+    @OneToMany(mappedBy = "profile")
+    private Set<Thread> threads;
+
+    /**
+     * Messages posted by this user.
+     */
+    @OneToMany(mappedBy = "profile")
+    private Set<Message> messages;
+
+    /**
+     * Geographic location associated with the user profile.
+     */
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    /**
+     * Default constructor required by JPA.
+     */
     public Profile() {}
 
+    /**
+     * Creates a user profile.
+     *
+     * @param id unique identifier of the profile
+     * @param firstName user's first name
+     * @param lastName user's last name
+     * @param pseudonym user's display name
+     * @param email user's email address
+     * @param city user's city
+     * @param postalCode user's postal code
+     * @param address user's address
+     * @param phoneNumber user's phone number
+     * @param linkImgProfile URL of the profile picture
+     * @param admin indicates whether the user has administrator privileges
+     */
     public Profile(
             UUID id,
             String firstName,
@@ -46,11 +121,10 @@ public class Profile {
             String pseudonym,
             String email,
             String city,
-            int postalCode,
+            String postalCode,
             String address,
             String phoneNumber,
             String linkImgProfile,
-            long meteoBookmarks,
             boolean admin) {
         this.id = id;
         this.firstName = firstName;
@@ -62,8 +136,28 @@ public class Profile {
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.linkImgProfile = linkImgProfile;
-        this.meteoBookmarks = meteoBookmarks;
         this.admin = admin;
     }
 
+    /**
+     * Returns a string representation of the profile.
+     *
+     * @return a string containing the profile information
+     */
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", pseudonym='" + pseudonym + '\'' +
+                ", email='" + email + '\'' +
+                ", city='" + city + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", linkImgProfile='" + linkImgProfile + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
 }
