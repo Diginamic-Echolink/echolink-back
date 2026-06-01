750c61df9f4c5608157d5035f49a0ea60377f1c8
Velki0 <b.gerardin125@gmail.com>
2026-06-01 10:40:16 +0200

    Initialisation

---
+++ src/main/java/fr/diginamic/echolink/EcholinkApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.echolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcholinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcholinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/diginamic/echolink/EcholinkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.echolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcholinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
