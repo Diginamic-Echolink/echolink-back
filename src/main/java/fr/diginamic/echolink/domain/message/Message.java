4632b766fea286abccbd5fd12538d4c8aea3729c
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-06-02 12:33:35 +0200

    JavaDoc: Create doc for Message,Location,Demography,AirQuality

--- src/main/java/fr/diginamic/echolink/domain/message/Message.java
+++ src/main/java/fr/diginamic/echolink/domain/message/Message.java
@@ -21,35 +21,50 @@ import java.util.UUID;
 @Table(name = "message")
 public class Message {
 
+    /** Unique identifier of the Message. */
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    /** Message's text */
     @Column(name="text", length = 10000)
     private String text;
 
+    /** Message's like count */
     @Column(name="like_count")
     private int likeCount;
 
+    /** Message's dislike count */
     @Column(name="dislike_count")
     private int dislikeCount;
 
+    /** Thread to which this message belongs */
     @ManyToOne
     @JoinColumn(name="thread_id")
     private Thread thread;
 
+    /** Profile that created the message */
     @ManyToOne
     @JoinColumn(name="profile_id")
     private Profile profile;
 
+    /** Constructor for: Message */
     public Message() {}
 
+    /**
+     * Constructor for: Message
+     *
+     * @param text
+     * @param likeCount
+     * @param dislikeCount
+     */
     public Message(String text, int likeCount, int dislikeCount) {
         this.text = text;
         this.likeCount = likeCount;
         this.dislikeCount = dislikeCount;
     }
 
+    /** @return toString */
     @Override
     public String toString() {
         return "Message{" +
