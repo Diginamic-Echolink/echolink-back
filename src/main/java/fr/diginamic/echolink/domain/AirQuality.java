c6cbc32bcc3723c1a802d68490e47a48712327fa
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-06-02 09:31:48 +0200

    Chore: Create of class Message,Location,Demography,AirQuality

--- src/main/java/fr/diginamic/echolink/domain/AirQuality.java
+++ src/main/java/fr/diginamic/echolink/domain/AirQuality.java
@@ -1,9 +1,124 @@
 package fr.diginamic.echolink.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class AirQuality {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "particles_10")
+    private int particles10;
+
+    @Column(name = "particles_25")
+    private int particles25;
+
+    @Column(name = "eu_aqi")
+    private int euAqi;
+
+    @Column(name = "mono_carbon")
+    private int monoCarbon;
+
+    @Column(name = "ozone")
+    private int ozone;
+
+    @Column(name = "dust")
+    private int dust;
+
+    @Column(name = "nitrogen_dioxide")
+    private int nitrogenDioxide;
+
+    @Column(name = "sulfur_dioxide")
+    private int sulfurDioxide;
+
+    @ManyToOne
+    @JoinColumn(name="location_id")
+    private Location location;
+
+    public AirQuality() {
+    }
+
+    public AirQuality(
+            UUID id,
+            LocalDateTime dateTime,
+            int particles10,
+            int particles25,
+            int euAqi,
+            int monoCarbon,
+            int ozone,
+            int dust,
+            int nitrogenDioxide,
+            int sulfurDioxide,
+            Location location
+    ) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.particles10 = particles10;
+        this.particles25 = particles25;
+        this.euAqi = euAqi;
+        this.monoCarbon = monoCarbon;
+        this.ozone = ozone;
+        this.dust = dust;
+        this.nitrogenDioxide = nitrogenDioxide;
+        this.sulfurDioxide = sulfurDioxide;
+        this.location = location;
+    }
+
+    public AirQuality(
+            UUID id,
+            LocalDateTime dateTime,
+            int particles10,
+            int particles25,
+            int euAqi,
+            int monoCarbon,
+            int ozone,
+            int dust,
+            int nitrogenDioxide,
+            int sulfurDioxide
+    ) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.particles10 = particles10;
+        this.particles25 = particles25;
+        this.euAqi = euAqi;
+        this.monoCarbon = monoCarbon;
+        this.ozone = ozone;
+        this.dust = dust;
+        this.nitrogenDioxide = nitrogenDioxide;
+        this.sulfurDioxide = sulfurDioxide;
+    }
+
+    @Override
+    public String toString() {
+        return "AirQuality{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", particles10=" + particles10 +
+                ", particles25=" + particles25 +
+                ", euAqi=" + euAqi +
+                ", monoCarbon=" + monoCarbon +
+                ", ozone=" + ozone +
+                ", dust=" + dust +
+                ", nitrogenDioxide=" + nitrogenDioxide +
+                ", sulfurDioxide=" + sulfurDioxide +
+                '}';
+    }
 }
--- src/main/java/fr/diginamic/echolink/domain/Message.java
+++ src/main/java/fr/diginamic/echolink/domain/Message.java
@@ -1,4 +1,71 @@
 package fr.diginamic.echolink.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name="text", length = 10000)
+    private String text;
+
+    @Column(name="likes_count")
+    private int likeCnt;
+
+    @Column(name="dislikes_count")
+    private int dislikeCnt;
+
+    @ManyToOne
+    @JoinColumn(name="thread_id")
+    private Thread thread;
+
+    @ManyToOne
+    @JoinColumn(name="profile_id")
+    private Profile profile;
+
+    public Message() {
+    }
+
+    public Message(UUID id, String text, int likeCnt, int dislikeCnt, Thread thread, Profile profile) {
+        this.id = id;
+        this.text = text;
+        this.likeCnt = likeCnt;
+        this.dislikeCnt = dislikeCnt;
+        this.thread = thread;
+        this.profile = profile;
+    }
+
+    public Message(int dislikeCnt, int likeCnt, String text, UUID id) {
+        this.dislikeCnt = dislikeCnt;
+        this.likeCnt = likeCnt;
+        this.text = text;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", likeCnt=" + likeCnt +
+                ", dislikeCnt=" + dislikeCnt +
+                ", thread=" + thread +
+                ", profile=" + profile +
+                '}';
+    }
 }
