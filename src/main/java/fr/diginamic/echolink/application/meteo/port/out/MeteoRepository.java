0f6ea32e40fe4eb300929383007f252c52b32d1c
Rohmane <R.S.T.E@hotmail.fr>
2026-06-03 14:25:20 +0200

    Feat: Meteo classes

---
+++ src/main/java/fr/diginamic/echolink/application/meteo/port/out/MeteoRepository.java
@@ -0,0 +1,14 @@
+package fr.diginamic.echolink.application.meteo.port.out;
+
+import fr.diginamic.echolink.domain.meteo.Meteo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MeteoRepository {
+
+    Optional<Meteo> getMeteoByLocationId(UUID id);
+
+    List<Meteo> getAllMeteoByLocationId(UUID locationId, int LIMIT_METEO);
+}
---
+++ src/main/java/fr/diginamic/echolink/infrastructure/meteo/in/dto/MeteoQuery.java
@@ -0,0 +1,15 @@
+package fr.diginamic.echolink.infrastructure.meteo.in.dto;
+
+import fr.diginamic.echolink.domain.meteo.WindDirection;
+
+import java.time.LocalDateTime;
+
+public record MeteoQuery(
+        LocalDateTime recordedAt,
+        byte temperature,
+        int atmPressure,
+        byte windSpeed,
+        WindDirection windDirection,
+        int rainFall,
+        byte humidity) {
+}
