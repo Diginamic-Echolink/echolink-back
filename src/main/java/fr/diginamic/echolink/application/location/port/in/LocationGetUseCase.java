838532c99e3f730c25c97f87e46670ef5c58cb7e
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-06-03 17:03:18 +0200

    Feat:Réalisation de la route du Controller -> Jdbc

--- src/main/java/fr/diginamic/echolink/application/location/port/in/LocationGetUseCase.java
+++ src/main/java/fr/diginamic/echolink/application/location/port/in/LocationGetUseCase.java
@@ -1,4 +1,13 @@
 package fr.diginamic.echolink.application.location.port.in;
 
+import fr.diginamic.echolink.domain.location.Location;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface LocationGetUseCase {
+
+    Location getById(UUID id);
+
+    List<Location> getByGeo(float latitude, float longitude, int delta);
 }
