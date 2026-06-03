db1d5d7838ad3a9701e99336a6d9e307ff6719f5
Rohmane <R.S.T.E@hotmail.fr>
2026-06-03 14:25:20 +0200

    Feat: Meteo classes

---
+++ src/main/java/fr/diginamic/echolink/application/meteo/service/MeteoGetService.java
@@ -0,0 +1,29 @@
+package fr.diginamic.echolink.application.meteo.service;
+
+
+import fr.diginamic.echolink.application.meteo.port.in.MeteoGetUseCase;
+import fr.diginamic.echolink.application.meteo.port.out.MeteoRepository;
+import fr.diginamic.echolink.domain.meteo.Meteo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MeteoGetService implements MeteoGetUseCase {
+
+    private final MeteoRepository repository;
+    private static final int LIMIT_METEO = 50;
+
+    @Override
+    public Meteo getMeteoByLocationId(UUID id) {
+        return repository.getMeteoByLocationId(id).orElse(null);
+    }
+
+    @Override
+    public List<Meteo> getAllMeteoByLocationId(UUID locationId) {
+        return repository.getAllMeteoByLocationId(locationId, LIMIT_METEO);
+    }
+}
---
+++ src/main/java/fr/diginamic/echolink/infrastructure/meteo/in/MeteoController.java
@@ -0,0 +1,44 @@
+package fr.diginamic.echolink.infrastructure.meteo.in;
+
+import fr.diginamic.echolink.application.meteo.port.in.MeteoGetUseCase;
+import fr.diginamic.echolink.domain.meteo.Meteo;
+import fr.diginamic.echolink.infrastructure.meteo.in.dto.MeteoQuery;
+import fr.diginamic.echolink.infrastructure.meteo.in.mapper.MeteoQueryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/meteo", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MeteoController {
+
+    private final MeteoGetUseCase meteoGetUseCase;
+    private final MeteoQueryMapper meteoQueryMapper;
+
+    @GetMapping("/{locationId}")
+    //@Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<MeteoQuery> getMeteoByLocationId(@PathVariable UUID locationId) {
+        Meteo meteos = meteoGetUseCase.getMeteoByLocationId(locationId);
+        MeteoQuery query = meteoQueryMapper.toQuery(meteos);
+        return ResponseEntity.ok(query);
+    }
+
+    @GetMapping("/all/{locationId}")
+    //@Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<List<MeteoQuery>> getAllMeteoByLocationId(@PathVariable UUID locationId) {
+        List<Meteo> meteos = meteoGetUseCase.getAllMeteoByLocationId(locationId);
+        List<MeteoQuery> query = meteos.stream().map(meteoQueryMapper::toQuery).toList();
+        return ResponseEntity.ok(query);
+    }
+
+
+}
