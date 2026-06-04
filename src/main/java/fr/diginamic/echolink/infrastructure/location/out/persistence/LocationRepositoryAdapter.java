36bb1af4017de18bfcc1a662a47d8f4deda0b410
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-06-04 12:10:55 +0200

    Fix: Correctif de la route du Controller -> Jdbc
    # Conflicts:
    #	src/main/java/fr/diginamic/echolink/application/location/port/out/LocationRepository.java
    #	src/main/java/fr/diginamic/echolink/application/location/service/LocationGetService.java
    #	src/main/java/fr/diginamic/echolink/infrastructure/location/in/LocationController.java
    #	src/main/java/fr/diginamic/echolink/infrastructure/location/in/dto/LocationQuery.java
    #	src/main/java/fr/diginamic/echolink/infrastructure/location/out/persistence/LocationRepositoryAdapter.java
    #	src/main/java/fr/diginamic/echolink/infrastructure/location/out/persistence/repository/LocationJdbcRepository.java
    
    # Conflicts:
    #	src/main/java/fr/diginamic/echolink/infrastructure/location/in/LocationController.java

--- src/main/java/fr/diginamic/echolink/infrastructure/location/out/persistence/LocationRepositoryAdapter.java
+++ src/main/java/fr/diginamic/echolink/infrastructure/location/out/persistence/LocationRepositoryAdapter.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
