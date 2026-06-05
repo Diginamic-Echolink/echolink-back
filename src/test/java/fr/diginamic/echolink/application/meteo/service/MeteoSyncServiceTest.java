bac355fed946a283ceb0f506df8e8a638280494c
Velki0 <b.gerardin125@gmail.com>
2026-06-05 16:19:07 +0200

    fix: for sonarqube

--- src/test/java/fr/diginamic/echolink/application/meteo/service/MeteoSyncServiceTest.java
+++ src/test/java/fr/diginamic/echolink/application/meteo/service/MeteoSyncServiceTest.java
@@ -27,7 +27,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MeteoSyncServiceTest {
+class MeteoSyncServiceTest {
 
     @Mock
     private LocationRepository locationRepository;
