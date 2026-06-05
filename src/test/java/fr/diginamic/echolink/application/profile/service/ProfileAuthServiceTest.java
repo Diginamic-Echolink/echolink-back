529208704ccc107ec67acacaa50f4b8d05b853e6
Velki0 <b.gerardin125@gmail.com>
2026-06-05 16:19:07 +0200

    fix: for sonarqube

--- src/test/java/fr/diginamic/echolink/application/profile/service/ProfileAuthServiceTest.java
+++ src/test/java/fr/diginamic/echolink/application/profile/service/ProfileAuthServiceTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ProfileAuthServiceTest {
+class ProfileAuthServiceTest {
 
     @Mock
     private ProfileRepository repository;
