aae6961aaadac71dd696f2d54cfd2abdae454ca0
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2026-06-10 17:52:07 +0200

    Remove MessageAccessDeniedException class

--- src/main/java/fr/diginamic/echolink/domain/message/exception/MessageAccessDeniedException.java
+++ src/main/java/fr/diginamic/echolink/domain/message/exception/MessageAccessDeniedException.java
@@ -1,7 +1,1 @@
-package fr.diginamic.echolink.domain.message.exception;
 
-public class MessageAccessDeniedException extends RuntimeException {
-    public MessageAccessDeniedException(String message) {
-        super(message);
-    }
-}
